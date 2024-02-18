d255ae281c80f4f05a3ec69fa860f6c96b73d76f
FLOPPY\Lisak <lisaklick08@gmail.com>
2024-02-18 19:27:02 +0100

    Enemy

--- Assets/workdata/scripts/Bullet.cs
+++ Assets/workdata/scripts/Bullet.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
-    }
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+           
+            Destroy(collision.gameObject);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+           
+        }
+        Destroy(this.gameObject);
     }
 }
